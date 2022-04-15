72406d3c1c6eef7e18c5d31a6eb93f128dd32b37
jw0293 <k38836919@gmail.com>
2022-04-15 13:36:16 +0900

    new file

---
+++ boj_#1351.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <climits>
+#include <set>
+#include <map> 
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define ll long long	
+#define ull unsigned long long
+#define INF 987654321
+#define Mod 1234567
+#define endl '\n'
+#define ENDL cout << endl
+
+using namespace std;
+
+ll N, Q, P;
+map<ll, ll> m;
+
+void input() {
+	cin >> N >> P >> Q;
+}
+
+ll path(ll n, ll p, ll q) {
+	if (n == 0) return 1;
+	ll& ret = m[n];
+	if (ret != 0) return ret;
+	return ret = path(n / p, p, q) + path(n / q, p, q);
+}
+
+ll solution() {
+	return path(N, P, Q);
+}
+
+int main() {
+	fastio;
+	input();
+	cout << solution() << endl;
+
+	return 0;
+}
