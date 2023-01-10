ee7bb6894d21e83aaf37cf04a096600c0b680532
jw0293 <max99kim@naver.com>
2023-01-11 00:37:54 +0900

    Implementation

--- boj_#20164.cpp
+++ boj_#20164.cpp
@@ -12,18 +12,26 @@ void input(){
 	cin >> N;
 }
 
-vector<int> getNumbers(string number){
-	vector<int> v;
-	for(int i=0;i<number.size()-1;i++){
-		v.push_back(i+1);
+int countingInitNumber(){
+	int cnt = 0;
+	for(int i=0;i<N.size();i++){
+		if((N[i] - '0') % 2 != 0) cnt++;
 	}
-	return v;
+	return cnt;
 }
 
-vector<int> getInit(int n){
-	vector<int> s(n,0);
-	for(int i=n-2;i<n;i++) s[i] = 1;
-	return s;
+int getOddNumber(string temp){
+	int count = 0;
+	for(int i=0;i<temp.size();i++){
+		if((temp[i] % 2 != 0)) {
+			count++;
+		}
+	}
+	return count;
+}
+
+int getSum(string s1, string s2, string s3){
+	return stoi(s1) + stoi(s2) + stoi(s3);
 }
 
 void func(string n, int cnt){
@@ -34,55 +42,28 @@ void func(string n, int cnt){
 	}
 
 	if(n.size() == 2){
-		int count = 0;
-		int a = n[0] - '0';
-		int b = n[1] - '0';
-		string temp = to_string(a+b);
-		for(int i=0;i<temp.size();i++){
-			if((temp[i]-'0') % 2 != 0) count++;
-		}
+		string temp = to_string((n[0]-'0') + (n[1]-'0'));
+		int count = getOddNumber(temp);
 		func(temp, cnt+count);
-	}else { 
-		int c = 0;
-		vector<int> ps, op;
-		for(int i=1;i<=n.size()-1;i++){
-			ps.push_back(i);
-			if(c < 2){
-				op.push_back(1);
-				c++;
-			} else{
-				op.push_back(0);
+	} else { 
+		for(int i=1;i<n.length()-1;i++){
+			for(int j=i+1;j<n.length();j++){
+				string s1 = n.substr(0, i);
+				string s2 = n.substr(i, j-i);
+				string s3 = n.substr(j, n.length()-j);
+
+				int sum = getSum(s1, s2, s3);
+				string temp = to_string(sum);
+				int count = getOddNumber(temp);
+
+				func(temp, cnt+count);
 			}
 		}
-		do{
-			vector<int> idx;
-			for(int i=0;i<ps.size();i++){
-				if(op[i] == 1){
-					idx.push_back(ps[i]);
-				}
-			}
-			string first = n.substr(0, idx[0]);
-			string second = n.substr(idx[0], idx[1]-idx[0]);
-			string third = n.substr(idx[1], idx[2]-idx[1]);
-
-			int sum = stoi(first) + stoi(second) + stoi(third);
-			string temp = to_string(sum);
-
-			int count = 0;
-			for(int i=0;i<temp.size();i++){
-				int x = temp[i]-'0';
-				if(x % 2 != 0) count++;
-			}
-			func(temp, cnt+count);
-		}while(prev_permutation(op.begin(), op.end()));
 	}
 }
 
 void solution(){
-	int cnt = 0;
-	for(int i=0;i<N.size();i++){
-		if((N[i] - '0') % 2 != 0) cnt++;
-	}
+	int cnt = countingInitNumber();
 	func(N, cnt);
 	cout << minV << " " << maxV << endl;
 }
