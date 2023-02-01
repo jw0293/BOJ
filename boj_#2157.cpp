92248fd91991c631e0f9dc25f108947c77353b3d
jw0293 <max99kim@naver.com>
2023-02-01 16:12:59 +0900

    Dynamic Programming

--- boj_#2157.cpp
+++ boj_#2157.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
-#define INF 123456789
+#define INF 1e9+7;
 #define endl '\n'
 
 using namespace std;
@@ -10,39 +10,34 @@ typedef unsigned long long ull;
 int N, M, K;
 int path[300 + 1][300 + 1];
 int cache[300 + 1][300 + 1];
-vector<int> include[300 + 1];
+vector<pair<int, int>> include[300 + 1];
 
 void input(){
 	cin >> N >> M >> K;
 	for(int i=0;i<K;i++){
 		int from, to, cost;
 		cin >> from >> to >> cost;
-
-		include[from].push_back(to);
-		path[from][to] = max(path[from][to], cost);
+		if(from > to) continue;
+		include[from].push_back({to, cost});
 	}
 }
 
-int dp(int start, int mid, int end, int cnt){
-	if(cnt > M) return -INF;
-	if(end == N && cnt <= M) return path[mid][end];
-	int &ret = cache[mid][end];
+int dp(int node, int cnt){
+	int &ret = cache[node][cnt];
 	if(ret != -1) return ret;
-	ret = 0;
-	for(int i=0;i<include[end].size();i++){
-		if(end > include[end][i]) continue;
-		ret = max(ret, dp(start, end, include[end][i], cnt+1) + path[mid][end]);
+	if(node == N) return ret = 0;
+	if(cnt >= M) return ret = -INF;
+	ret = -INF;
+	for(auto &next : include[node]){
+		if(node > next.first) continue;
+		ret = max(ret, dp(next.first, cnt+1) + next.second);
 	}
 	return ret;
 }
 
 int solution(){
-	int answer = 0;
 	memset(cache, -1, sizeof(cache));
-	for(int i=0;i<include[1].size();i++){
-		answer = max(answer, dp(1, 1, include[1][i], 2));
-	}
-	return answer;
+	return dp(1, 1);
 }
 
 int main(){
