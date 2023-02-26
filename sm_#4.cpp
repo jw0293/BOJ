ddbb2d262ee77d4d9ec5303de00a96c0628880ae
jw0293 <max99kim@naver.com>
2023-02-26 23:54:07 +0900

    #4

--- sm_#4.cpp
+++ sm_#4.cpp
@@ -20,12 +20,13 @@ vector<string> input(){
 	return cps;
 }
 
-pair<int, int> getIndex(int idx, char op, vector<string> &cps){
+pair<int, int> getIndex(int idx, int pos, vector<string> &cps){
+	char op = cps[idx][pos];
 	for(int i=idx+1;i<cps.size();i++){
 		if(cps[i][0] == op) return {i, 0};
 		if(cps[i][1] == op) return {i, 1};
 	}
-	return {idx, 0};
+	return {idx, pos};
 }
 
 int getCountSwap(pair<int, int> purpose, pair<int, int> cur,vector<string> &cps){
@@ -37,28 +38,25 @@ int getCountSwap(pair<int, int> purpose, pair<int, int> cur,vector<string> &cps)
 			break;
 		}
 
-		pair<int, int> prevLeft = getIndex(cur.first-1, cps[cur.first-1][0], cps);
-		pair<int, int> prevRight = getIndex(cur.first-1, cps[cur.first-1][1], cps);
-
-		cout << "PREVLEFT : [ " << prevLeft.first << ", " << prevLeft.second << " ]" << endl;
-		cout << "PREVRIGHT : [ " << prevRight.first << ", " << prevRight.second << " ]" << endl;
+		pair<int, int> prevLeft = getIndex(cur.first-1, 0, cps);
+		pair<int, int> prevRight = getIndex(cur.first-1, 1, cps);
 
 		if(prevLeft.first == prevRight.first){
-			swap(cps[prevLeft.first][(prevLeft.second+1)%2], cps[cur.first][cur.second]);
+			swap(cps[cur.first-1][0], cps[cur.first][cur.second]);
 
-			cur.first = prevLeft.first;
-			cur.second = (prevLeft.second+1)%2;
+			cur.first = cur.first-1;
+			cur.second = 0; 
 		} else{
 			if(prevLeft.first < prevRight.first) {
-				swap(cps[prevRight.first][(prevRight.second+1)%2], cps[cur.first][cur.second]);
+				swap(cps[cur.first-1][1], cps[cur.first][cur.second]);
 
-				cur.first = prevRight.first;
-				cur.second = (prevRight.second+1)%2;
+				cur.first = cur.first-1;
+				cur.second = 1;
 			} else{
-				swap(cps[prevLeft.first][(prevLeft.second+1)%2], cps[cur.first][cur.second]);
+				swap(cps[cur.first-1][0], cps[cur.first][cur.second]);
 				
-				cur.first = prevLeft.first;
-				cur.second = (prevLeft.second+1)%2;
+				cur.first = cur.first-1;
+				cur.second = 0;
 			}
 		}
 		count++;
@@ -71,10 +69,8 @@ int solution(vector<string> &chopsticks){
 	for(int i=0;i<chopsticks.size()-1;i++){
 		if(chopsticks[i][0] == chopsticks[i][1]) continue;
 
-		for(auto &c : chopsticks) cout << "[ " << c[0] << ":" << c[1] << " ]" << endl;
-
-		pair<int, int> left = getIndex(i, chopsticks[i][0], chopsticks);
-		pair<int, int> right = getIndex(i, chopsticks[i][1], chopsticks);
+		pair<int, int> left = getIndex(i, 0, chopsticks);
+		pair<int, int> right = getIndex(i, 1, chopsticks);
 
 		if(left.first == right.first){
 			answer += getCountSwap({i,0}, left, chopsticks);
@@ -82,7 +78,6 @@ int solution(vector<string> &chopsticks){
 			if(left.first < right.first) answer += getCountSwap({i,0}, left, chopsticks);
 			else answer += getCountSwap({i,1}, right, chopsticks);
 		}
-		cout << endl;
 	}
 	return answer;
 }
